5b28585b55d81efa442176ccb2223bb1a06faa3d
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-02-17 23:31:57 +0000

    Mapa criado

--- tabbedApp/FirstViewController.swift
+++ tabbedApp/FirstViewController.swift
@@ -1,22 +1,28 @@
-//
-//  FirstViewController.swift
-//  tabbedApp
-//
-//  Created by Sebastian Hette on 07.11.2016.
-//  Copyright © 2016 MAGNUMIUM. All rights reserved.
-//
+
 
 import UIKit
+import MapKit
 
 class FirstViewController: UIViewController {
 
-    @IBOutlet weak var webview: UIWebView!
+  
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let url = NSURL (string: "https://neec-fct.com/demoappp/equipa/team/equipa.html");
-        let requestObj = NSURLRequest(url: url! as URL);
-        webview.loadRequest(requestObj as URLRequest);
+        
+        let span:MKCoordinateSpan = MKCoordinateSpanMake( 0.005 , 0.005)
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(38.661307,  -9.207192)
+        
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Ponto 1"
+        annotation.subtitle = "Algo"
+        map.addAnnotation(annotation)
+        
     }
 
     override func didReceiveMemoryWarning() {
