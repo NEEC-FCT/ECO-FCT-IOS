c970d797bb9de046871366031e7cf3ebaa939c1a
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-02-18 01:17:49 +0000

    Create Login.swift

---
+++ tabbedApp/Login.swift
@@ -0,0 +1,28 @@
+//
+//  Login.swift
+//  ECO FCT
+//
+//  Created by NEEC on 18/02/2019.
+//  Copyright © 2019 MAGNUMIUM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Login: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
