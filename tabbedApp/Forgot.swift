ef391b68163bcf89c1452f8ff28a0aa89c313f14
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-02-18 01:17:53 +0000

    Create Forgot.swift

---
+++ tabbedApp/Forgot.swift
@@ -0,0 +1,29 @@
+//
+//  Forgot.swift
+//  ECO FCT
+//
+//  Created by NEEC on 18/02/2019.
+//  Copyright © 2019 MAGNUMIUM. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Forgot: UIViewController {
+    
+
+   override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
