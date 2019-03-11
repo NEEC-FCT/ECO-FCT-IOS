7c3e004242903bfd827b31ff89b2bd5c79f8a045
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-03-11 03:31:36 +0000

    Login Working

--- tabbedApp/Login.swift
+++ tabbedApp/Login.swift
@@ -11,9 +11,101 @@ import UIKit
 
 class Login: UIViewController {
     
+    @IBOutlet weak var emailtxt: UITextField!
+    @IBOutlet weak var passtxt: UITextField!
     
+    
+    func convertToDictionary(text: String) -> [String: Any]? {
+        if let data = text.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
+    
+    
+    @IBAction func loginClicked(_ sender: Any) {
+        
+        NSLog("clicou")
+        
+        
+        let url = URL(string: "https://ecoapp.neec-fct.com/Login.php")!
+        var request = URLRequest(url: url)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        let postString = "username=" + emailtxt.text! + "&password=" + passtxt.text!
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                
+                
+                print("error=\(error!)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                
+                
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response!)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            
+            let dict = self.convertToDictionary(text: responseString!)
+            
+            //login
+            if( dict?["success"]  as! Int == 1 ){
+                print( "Vou mudar " )
+                
+                UserDefaults.standard.set(dict?["email"]  as! String, forKey: "EMAIL")
+                UserDefaults.standard.set( dict?["token"]  as! String, forKey: "TOKEN")
+                
+                //go to storyboard
+                DispatchQueue.main.async {
+                    let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                    let newViewController = storyBoard.instantiateViewController(withIdentifier: "starter")
+                    self.present(newViewController, animated: true, completion: nil)
+                }
+                
+                
+                
+            }
+                //Erro
+            else{
+                DispatchQueue.main.async {
+                    let alert = UIAlertController(title: "Error", message: "Check your password", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+                        switch action.style{
+                        case .default:
+                            print("default")
+                            
+                        case .cancel:
+                            print("cancel")
+                            
+                        case .destructive:
+                            print("destructive")
+                            
+                            
+                        }}))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+            print("responseString = \(responseString!)")
+        }
+        task.resume()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let defaults = UserDefaults.standard
+        let dictionary = defaults.dictionaryRepresentation()
+        dictionary.keys.forEach { key in
+            defaults.removeObject(forKey: key)
+        }
         
         
         // Do any additional setup after loading the view, typically from a nib.
