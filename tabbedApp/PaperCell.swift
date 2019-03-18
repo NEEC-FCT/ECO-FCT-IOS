679b36ea0408cb6824e70713ffec0599b07a1793
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-03-18 17:16:20 +0000

    Paper menu

---
+++ tabbedApp/PaperCell.swift
@@ -0,0 +1,15 @@
+//
+//  PaperCell.swift
+//  ECO FCT
+//
+//  Created by NEEC on 18/03/2019.
+//  Copyright © 2019 MAGNUMIUM. All rights reserved.
+//
+
+import UIKit
+
+
+class PaperCell: UICollectionViewCell {
+    
+     @IBOutlet weak var myLabel: UILabel!
+}
--- tabbedApp/PaperChoosed.swift
+++ tabbedApp/PaperChoosed.swift
@@ -1,9 +1,75 @@
 //
-//  PaperChoosed.swift
-//  ECO FCT
+//  SecondViewController.swift
+//  tabbedApp
 //
-//  Created by NEEC on 18/03/2019.
-//  Copyright © 2019 MAGNUMIUM. All rights reserved.
+//  Created by Sebastian Hette on 07.11.2016.
+//  Copyright © 2016 MAGNUMIUM. All rights reserved.
 //
 
-import Foundation
+
+
+
+import UIKit
+
+class  PaperChoosed: UIViewController  , UICollectionViewDataSource, UICollectionViewDelegate  , UICollectionViewDelegateFlowLayout{
+    
+    
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+    var descricao = ["Papel ou Cartão", "Plástico ou Metal", "Vidro", "Resíduo Perigoso", "Resíduo de Elétricos", "Lâmpada", "Óleo lubrificante", "Mobiliário", "Resíduo Orgânico" ]
+    
+    
+    // MARK: - UICollectionViewDataSource protocol
+    
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.descricao.count
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! PaperCell
+        
+        //Set na celu
+        cell.myLabel.text = self.descricao[indexPath.item]
+        cell.myLabel.backgroundColor = UIColor(hexString: "#e5f71b" )
+
+        
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        print("You selected cell #\(indexPath.item)!")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let padding: CGFloat =  10
+        let collectionViewSize = collectionView.frame.size.width - padding
+        
+        
+        
+        return CGSize(width: collectionViewSize/2, height: collectionViewSize/4)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+    
+    
+    
+}
+
