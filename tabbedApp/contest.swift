e73f4b3055351609f2f8acc3f3cd0bdac4df276d
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-03-18 01:22:21 +0000

    ListView for ranking

--- tabbedApp/contest.swift
+++ tabbedApp/contest.swift
@@ -11,9 +11,24 @@ import UIKit
 import AVFoundation
 
 
-class contest: UIViewController {
+class contest: UIViewController , UITableViewDelegate , UITableViewDataSource {
     
   
+    let list = ["Milk", "Honey", "Bread", "Tacos", "Tomatoes"]
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return(list.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        
+        return(cell)
+    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
